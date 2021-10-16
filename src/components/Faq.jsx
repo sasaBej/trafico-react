92c0773b8fc603add1653783de573be526de1195
Alexandru Bejenari <alexandru.bejenari.falesti@gmail.com>
2021-10-16 16:59:00 +0300

    add postItem

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 import faqImg from '../assets/img/bottom_guy.svg';
+import PostItem from './PostItem';
+
+async function fetchComments() {
+    const response = await axios.get("https://lxpi9qne2a.api.quickmocker.com/getReactFAQs");
+    console.log(response.data.response);
+}
+fetchComments();
 
 function Faq() {
     return (
@@ -10,15 +18,13 @@ function Faq() {
                     <div className="faq__content-q">
                         <h2>faq</h2>
                         <p>Questions and Answers on Professional Traffic Permits:</p>
-                        
-
                     </div>
                     <div className="faq__img-guy">
                         <img src={faqImg} alt="Image Guy Question" />
                     </div>
                 </div>               
                 <div className="question_com"> 
-
+                    <PostItem/>
                 </div>
            </div>
         </section>
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="post__question">
+                <p>What is a professional traffic permit?</p>
+                <div className="post__question-sign">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div className="post__answer">
+                Traffic permits are a requirement for conducting professional traffic.
+            </div>
+        </div>
+    )
+}
+
+export default PostItem;
